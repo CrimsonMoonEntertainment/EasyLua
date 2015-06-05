be3a603ecf7687e5d9d839bd9f1a5c96b4f618cd
David Holtkamp <david@crimson-moon.com>
2015-06-05 13:45:13 -0400

    Changed braces to use KVC vs just working with Dictionaries

--- Pod/Assets/LuaBridge.lua
+++ Pod/Assets/LuaBridge.lua
@@ -35,7 +35,7 @@ function wrap(obj)
                                 end
                              end,
                     __index = function(inObject, inKey)
-                                local ret = sendMesg(inObject["WrappedObject"], 'objectForKey:', inKey)
+                                local ret = sendMesg(inObject["WrappedObject"], 'valueForKey:', inKey)
                                 if type(ret) == "userdata" then
                                    return wrap(ret)
                                 else
@@ -43,7 +43,7 @@ function wrap(obj)
                                 end
                               end,
                     __newindex =  function(inObject, inKey, inValue)
-                                    sendMesg(inObject["WrappedObject"], 'setObject:forKey:', inValue, inKey)
+                                    sendMesg(inObject["WrappedObject"], 'setValue:forKey:', inValue, inKey)
                                   end
 
 
